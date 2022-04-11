0ba308945f43955ff88dc6f0f5c822d2abe3c2a4
Mark R <murkrobles@gmail.com>
2022-04-10 20:03:59 -0700

    additional comments

--- src/Filters.java
+++ src/Filters.java
@@ -28,15 +28,25 @@ public class Filters {
 	public static final ArrayList<Filters> FILTERS = new ArrayList<>();
 
 	static {
+		// Protanopia, Deuteranopia, Tritanopia values
 		FILTERS.add(new Filters("Protanopia", new float[][] { 
 			{ 0.567f, 0.433f, 0f }, { 0.558f, 0.442f, 0f }, { 0f, 0.242f, 0.758f } }));	
-		FILTERS.add(new Filters("Deuteranopia", new float[][] { { 0.625f, 0.375f, 0f }, { 0.458f, 0.442f, 0f }, { 0f, 0.3f, 0.7f } }));
-		FILTERS.add(new Filters("Tritanopia", new float[][] { { 0.95f, 0.05f, 0f }, { 0f, 0.433f, 0.567f }, { 0f, 0.475f, 0.525f } }));
-
 		
-		FILTERS.add(new Filters("Protanomaly", new float[][] { { 0.817f, 0.183f, 0f }, { 0.333f, 0.667f, 0f }, { 0f, 0.125f, 0.875f } }));
-		FILTERS.add(new Filters("Deuteranomaly", new float[][] { { 0.8f, 0.2f, 0f }, { 0.258f, 0.742f, 0f }, { 0f, 0.142f, 0.858f } }));
-		FILTERS.add(new Filters("Tritanomaly", new float[][] { { 0.967f, 0.033f, 0f }, { 0f, 0.733f, 0.267f }, { 0f, 0.183f, 0.817f } }));
+		FILTERS.add(new Filters("Deuteranopia", new float[][] { 
+			{ 0.625f, 0.375f, 0f }, { 0.458f, 0.442f, 0f }, { 0f, 0.3f, 0.7f } }));
+		
+		FILTERS.add(new Filters("Tritanopia", new float[][] { 
+			{ 0.95f, 0.05f, 0f }, { 0f, 0.433f, 0.567f }, { 0f, 0.475f, 0.525f } }));
+
+		// Protanopaly, Deuteranopaly, Tritanomaly values
+		FILTERS.add(new Filters("Protanomaly", new float[][] { 
+			{ 0.817f, 0.183f, 0f }, { 0.333f, 0.667f, 0f }, { 0f, 0.125f, 0.875f } }));
+		
+		FILTERS.add(new Filters("Deuteranomaly", new float[][] 
+				{ { 0.8f, 0.2f, 0f }, { 0.258f, 0.742f, 0f }, { 0f, 0.142f, 0.858f } }));
+		
+		FILTERS.add(new Filters("Tritanomaly", new float[][] { {
+			0.967f, 0.033f, 0f }, { 0f, 0.733f, 0.267f }, { 0f, 0.183f, 0.817f } }));
 	}
 	
 	public static BufferedImage filterImage(BufferedImage src, Filters f) {
@@ -88,14 +98,25 @@ public class Filters {
 	public static final ArrayList<Filters> FIXEDFILTERS = new ArrayList<>();
 
 	static {
-		FIXEDFILTERS.add(new Filters("ProtanopiaFixed", new float[][] { { 0.8f, 0.1f, 0f }, { 0.8f, 0.2f, 0f }, { 0.1f, 0.6f, 0.2f } }));
-		FIXEDFILTERS.add(new Filters("DeuteranopiaFixed", new float[][] { { 0.925f, 0.075f, 0f }, { 0.558f, 0.142f, 0.2f }, { 0f, 0.8f, 0.2f } }));
-		FIXEDFILTERS.add(new Filters("TritanopiaFixed", new float[][] { { 0.05f, 0.95f, 0f }, { 0f, 0.133f, 0.867f }, { 0f, 0.475f, 0.525f } }));
-
+		// Fixed Protanopia, Deuteranopia, Tritanopia values
+		FIXEDFILTERS.add(new Filters("ProtanopiaFixed", new float[][] { 
+			{ 0.8f, 0.1f, 0f }, { 0.8f, 0.2f, 0f }, { 0.1f, 0.6f, 0.2f } }));
 		
-		FIXEDFILTERS.add(new Filters("ProtanomalyFixed", new float[][] { { 0.8f, 0.1f, 0f }, { 0.8f, 0.2f, 0f }, { 0.1f, 0.6f, 0.2f } }));
-		FIXEDFILTERS.add(new Filters("DeuteranomalyFixed", new float[][] { { 0.925f, 0.075f, 0f }, { 0.558f, 0.142f, 0.2f }, { 0f, 0.8f, 0.2f } }));
-		FIXEDFILTERS.add(new Filters("TritanomalyFixed", new float[][] { { 0.05f, 0.95f, 0f }, { 0f, 0.133f, 0.867f }, { 0f, 0.475f, 0.525f } }));
+		FIXEDFILTERS.add(new Filters("DeuteranopiaFixed", new float[][] { 
+			{ 0.925f, 0.075f, 0f }, { 0.558f, 0.142f, 0.2f }, { 0f, 0.8f, 0.2f } }));
+		
+		FIXEDFILTERS.add(new Filters("TritanopiaFixed", new float[][] { 
+			{ 0.05f, 0.95f, 0f }, { 0f, 0.133f, 0.867f }, { 0f, 0.475f, 0.525f } }));
+
+		// Fixed Protanopaly, Deuteranopaly, Tritanomaly values
+		FIXEDFILTERS.add(new Filters("ProtanomalyFixed", new float[][] { 
+			{ 0.8f, 0.1f, 0f }, { 0.8f, 0.2f, 0f }, { 0.1f, 0.6f, 0.2f } }));
+		
+		FIXEDFILTERS.add(new Filters("DeuteranomalyFixed", new float[][] 
+				{ { 0.925f, 0.075f, 0f }, { 0.558f, 0.142f, 0.2f }, { 0f, 0.8f, 0.2f } }));
+		
+		FIXEDFILTERS.add(new Filters("TritanomalyFixed", new float[][] { 
+			{ 0.05f, 0.95f, 0f }, { 0f, 0.133f, 0.867f }, { 0f, 0.475f, 0.525f } }));
 	}
 	
 	public static BufferedImage colorCorrectedFilter(BufferedImage src, Filters f) {
